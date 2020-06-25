4d74bd0cf3a87e95775af4e55fe86fcf4f95fed6
kimjunseok <jnunseok@gmail.com>
2020-06-25 15:58:05 +0900

    feat: controller

---
+++ src/main/java/controller/ChickenController.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class ChickenController {
+
+    private static final ChickenController chickenController = new ChickenController();
+
+    private ChickenController() {}
+
+    public static ChickenController getInstance() {
+        return chickenController;
+    }
+
+    public void execute() {
+
+    }
+}
